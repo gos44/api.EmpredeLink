68caf65b2c2c92037d6ba5e1a4c8a2af923ce75d
sh4ka11 <dayronhernandezparedes@gmail.com>
2024-08-21 10:18:32 -0500

    modelo emprendimiento

--- app/Http/Controllers/api/EmprendimientoController.php
+++ app/Http/Controllers/api/EmprendimientoController.php
@@ -11,8 +11,19 @@ class EmprendimientoController extends RoutingController
 {
     public function index()
     {
-        $emprendimiento = Emprendimiento::all();
-        return response()->json($emprendimiento);
+        $emprendimientos = Emprendimiento::all(); 
+        // Puedes usar filtros, ordenamientos, etc. como se muestra en los ejemplos comentados:
+        // $emprendimientos = Emprendimiento::included()->get();
+        // $emprendimientos = Emprendimiento::included()->filter();
+        // $emprendimientos = Emprendimiento::included()->filter()->sort()->get();
+        // $emprendimientos = Emprendimiento::included()->filter()->sort()->getOrPaginate();
+
+        return response()->json($emprendimientos);
+    }
+
+    public function create()
+    {
+        return view('Emprendimiento.create');
     }
 
     public function store(Request $request)
@@ -30,8 +41,18 @@ class EmprendimientoController extends RoutingController
 
     public function show($id)
     {
+        // Se pueden incluir relaciones en la consulta, como se muestra en los ejemplos comentados:
+        // $emprendimiento = Emprendimiento::with(['relacion1', 'relacion2'])->findOrFail($id);
+        // $emprendimiento = Emprendimiento::included()->findOrFail($id);
+
         $emprendimiento = Emprendimiento::findOrFail($id);
         return response()->json($emprendimiento);
+        // Ejemplo de ruta: http://api.ejemplo.test/v1/emprendimientos/1/?included=relacion1,relacion2
+    }
+
+    public function edit(Emprendimiento $emprendimiento)
+    {
+        return view('Emprendimiento.edit', compact('emprendimiento'));
     }
 
     public function update(Request $request, Emprendimiento $emprendimiento)
@@ -40,7 +61,7 @@ class EmprendimientoController extends RoutingController
             'nombre_emprendimiento' => 'required|max:255',
             'descripcion' => 'required|max:255',
             'especificaciones' => 'required|max:255',
-            'categoria' => 'required|max:255'.$emprendimiento->id,
+            'categoria' => 'required|max:255',
         ]);
 
         $emprendimiento->update($request->all());
@@ -53,3 +74,4 @@ class EmprendimientoController extends RoutingController
         return response()->json(null, 204);
     }
 }
+
