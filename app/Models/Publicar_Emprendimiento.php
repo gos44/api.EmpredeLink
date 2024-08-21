8347dd68d264e6d76478b98804277dba0ed2609a
jhon azcarate <trejos19>
2024-08-21 11:34:42 -0500

    v1.001

--- app/Models/Publicar_Emprendimiento.php
+++ app/Models/Publicar_Emprendimiento.php
@@ -6,56 +6,42 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
-
 class Publicar_Emprendimiento extends Model
 {
     use HasFactory;
 
-    public function emprendimientos (){
-        return $this->belongsTo(emprendimiento::class);
+    protected $fillable = ['name', 'last_name', 'phone_number', 'mail', 'description', 'location', 'url', 'date_exp'];
+
+    protected $allowIncluded = ['emprendedores', 'emprendimiento'];
+    protected $allowFilter = ['id', 'name', 'lastname', 'correo']; // Campos permitidos para filtrar.
+    protected $allowSort = ['id', 'name', 'lastname', 'correo']; // Campos permitidos para ordenar.
+
+
+    public function emprendimiento()
+    {
+        return $this->belongsTo(Emprendimiento::class);
     }
 
-    public function emprendedors (){
-        return $this->hasMany(emprendedor::class);
+    public function emprendedores()
+    {
+        return $this->hasMany(Emprendedor::class);
     }
-
-
-    protected $allowIncluded = ['emprendedors', 'emprendedors.resenas', 'inversionistas', 'resenas', 'publicar_emprendimiento', 'Conexion', 'categorias', 'users'];//las posibles Querys que se pueden realizar
-
-    protected $fillable = ['name', 'last_name','phone_number','mail','description','location','url','date_exp'];
-    //Campos que se van a asignacion masiva:
-
 
     public function scopeIncluded(Builder $query)
     {
-
-        if(empty($this->allowIncluded)||empty(request('included'))){
-            // validamos que la lista blanca y la variable included enviada a travez de HTTP no este en vacia.
+        if (empty($this->allowIncluded) || empty(request('included'))) {
             return;
         }
 
-        
         $relations = explode(',', request('included'));
-         //['posts','relation2']//recuperamos el valor de la variable included y separa sus valores por una coma
-         // return $relations;
-
         $allowIncluded = collect($this->allowIncluded);
-         //colocamos en una colecion lo que tiene $allowIncluded en este caso = ['posts','posts.user']
 
         foreach ($relations as $key => $relationship) {
-            //recorremos el array de relaciones
-
             if (!$allowIncluded->contains($relationship)) {
                 unset($relations[$key]);
             }
         }
+
         $query->with($relations);
-         //se ejecuta el query con lo que tiene $relations en ultimas es el valor en la url de included
-
-        //http://api.EmpredeLink/api/categories?included=posts
-
-
     }
-
-    }
-
+}
